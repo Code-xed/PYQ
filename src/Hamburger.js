f5afb11d1fe2687a5882f47434bf575e5124a6f5
Code-xed <ngudulu@gmail.com>
2024-02-22 22:54:53 +0530

    Initial commit

---
+++ src/Hamburger.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./style.css";
+
+function Hamburger() {
+    return(
+        <>
+        <div className='line'></div>
+        <div className='line'></div>
+        <div className='line'></div>
+        </>
+    );
+};
+
+export default Hamburger;
---
+++ src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hamburger from './Hamburger';
+import "./style.css";
+const HelloWorld = () => {
+  return (
+    <>
+      <Hamburger classname="hamburger"/>
+      <h3>Hello World</h3>
+    </>
+  );
+};
+
+export default HelloWorld;
